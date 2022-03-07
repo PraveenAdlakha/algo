c6610a452531a2fe0d8010804010950cd8612bf5
Praveen Adlakha <padlakha@atlassian.com>
2022-03-07 20:15:38 +0530

    vwap selling code added

--- vwap_based_straddle_selling.py
+++ vwap_based_straddle_selling.py
@@ -62,17 +62,62 @@ def getQuote(tradingSymbol):
     else:
         return 0
 
-def sellBankNiftyStraddleAndMonitor(ATMStrike):
+def sellBankNiftyStraddleAndMonitor( inst_name, qty):
+    ATMStrike = getNearestStrikePrice(getCMP('NSE:NIFTY BANK'), 100)
     bankNiftyCE = getQuote("NFO:"+ str(ATMStrike))
     df = pd.read_csv("NFO_Instruments.csv")
     print(df.head())
-    next_thursday_expiry = datetime.today() + relativedelta(weekday=TH(1))
+    next_thursday_expiry = (datetime.today() + relativedelta(weekday=TH(1))).date()
+    df1 = df[(df.instrument_type == 'PE' )& (df.name == inst_name) & (df.strike == ATMStrike) & (df.expiry == str(next_thursday_expiry))]
+    pe_trading_symbol = ''
+    ce_trading_symbol = ''
 
+    for row in df1.itertuples():
+        pe_trading_symbol = getattr(row, 'tradingsymbol')
+
+    df1 = df[(df.instrument_type == 'PE') & (df.name == inst_name) & (df.strike == ATMStrike) & (
+                df.expiry == str(next_thursday_expiry))]
+
+    for row in df1.itertuples():
+        ce_trading_symbol = getattr(row, 'tradingsymbol')
+
+    quote_ce = getQuote("NFO:" + ce_trading_symbol)
+    quote_pe = getQuote("NFO:" + pe_trading_symbol)
+
+    avg_quote_ce_price = quote_ce["NFO:"+ ce_trading_symbol]['average_price']
+    avg_quote_pe_price = quote_pe["NFO:"+ pe_trading_symbol]['average_price']
+
+    cmp_ce = getCMP("NFO:" + ce_trading_symbol)
+    cmp_pe = getCMP("NFO:" + pe_trading_symbol)
+
+    if(cmp_ce+ cmp_pe +10 < avg_quote_pe_price+ avg_quote_ce_price):
+        place_order(pe_trading_symbol,0, qty, kite.TRANSACTION_TYPE_SELL, KiteConnect.EXCHANGE_NFO, KiteConnect.PRODUCT_NRML,
+                    KiteConnect.ORDER_TYPE_MARKET)
+        place_order(ce_trading_symbol,0, qty, kite.TRANSACTION_TYPE_SELL, KiteConnect.EXCHANGE_NFO, KiteConnect.PRODUCT_NRML,
+                    KiteConnect.ORDER_TYPE_MARKET)
+        while(True):
+            time.sleep(2)
+            cmp_ce = getCMP("NFO:" + avg_quote_ce_price)
+            cmp_pe = getCMP("NFO:" + avg_quote_pe_price)
+            quote_ce = getQuote("NFO:" + ce_trading_symbol)
+            quote_pe = getQuote("NFO:" + pe_trading_symbol)
+
+            avg_quote_ce_price = quote_ce["NFO:" + ce_trading_symbol]['average_price']
+            avg_quote_pe_price = quote_pe["NFO:" + pe_trading_symbol]['average_price']
+
+            if(cmp_ce+ cmp_pe  > avg_quote_pe_price+ avg_quote_ce_price + 5):
+                place_order(pe_trading_symbol,0, qty, kite.TRANSACTION_TYPE_BUY, KiteConnect.EXCHANGE_NFO,
+                            KiteConnect.PRODUCT_NRML,
+                            KiteConnect.ORDER_TYPE_MARKET)
+                place_order(ce_trading_symbol,0, qty, kite.TRANSACTION_TYPE_BUY, KiteConnect.EXCHANGE_NFO,
+                            KiteConnect.PRODUCT_NRML,
+                            KiteConnect.ORDER_TYPE_MARKET)
 
 
 
 if __name__ == '__main__':
     # We will sell straddle when its below vwap and do some trailing to bring sl to cost
     # exit when you are in certain profit of 40-50 points it should give better return on wednesday and thursday
-    BankNiftyATMStrike = getNearestStrikePrice(getCMP('NSE:NIFTY BANK'),100)
-    sellStraddleAndMonitor = sellBankNiftyStraddleAndMonitor(BankNiftyATMStrike)
+
+    qty = 200
+    sellStraddleAndMonitor = sellBankNiftyStraddleAndMonitor('BANKNIFTY', qty)
